e7494d16779e3abae3104c6001f7199e47c16f4f
Rino Santoso <43334126+rinoSantoso@users.noreply.github.com>
2020-06-06 23:06:09 +0700

    Added diceRoll() function

--- Utils.py
+++ Utils.py
@@ -9,4 +9,10 @@ def turnRandomizer(p1, p2):
         print("Player 2's Turn")
         return p2
 
-
+def diceRoll():
+    randomNum1 = random.randint(0,1)
+    randomNum2 = random.randint(0, 1)
+    randomNum3 = random.randint(0, 1)
+    randomNum4 = random.randint(0, 1)
+    total = randomNum1 + randomNum2 + randomNum3 + randomNum4
+    return total
